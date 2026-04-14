203b8676443fbc1fe7694f93f2f4e8d96bac7be9
atlet13 <artmatveev113@gmail.com>
2026-04-15 00:54:15 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import asyncio
-from aiogram import Bot, Dispatcher, types
-from aiogram.types import WebAppInfo, ReplyKeyboardMarkup, KeyboardButton
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message, WebAppInfo, ReplyKeyboardMarkup, KeyboardButton
+from aiogram.filters import Command
 
 TOKEN = "8659665258:AAFyL7OZBZqkU-D7iaE2auHyf73KdYyeeIM"
 WEBAPP = "https://casino-bot-1-p7mp.onrender.com"
@@ -8,17 +9,21 @@ WEBAPP = "https://casino-bot-1-p7mp.onrender.com"
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
-@dp.message(commands=["start"])
-async def start(msg: types.Message):
+@dp.message(Command("start"))
+async def start(msg: Message):
     kb = ReplyKeyboardMarkup(
         keyboard=[
-            [KeyboardButton(text="🎰 Грати", web_app=WebAppInfo(url=WEBAPP))]
+            [KeyboardButton(
+                text="🎰 Грати",
+                web_app=WebAppInfo(url=WEBAPP)
+            )]
         ],
         resize_keyboard=True
     )
     await msg.answer("🎰 Відкрий гру", reply_markup=kb)
 
 async def main():
+    print("Bot started...")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
